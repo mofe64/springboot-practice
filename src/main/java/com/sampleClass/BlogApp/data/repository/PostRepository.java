063988868a30182148569351c64402f6aeb99567
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-05-31 10:21:39 +0100

    post repo impl

--- src/main/java/com/sampleClass/BlogApp/data/repository/PostRepository.java
+++ src/main/java/com/sampleClass/BlogApp/data/repository/PostRepository.java
@@ -2,10 +2,19 @@ package com.sampleClass.BlogApp.data.repository;
 
 import com.sampleClass.BlogApp.data.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Post findByTitle(String title);
 
+    @Query("select  p from Post p where p.title = :title")
+    Post findByPostTitle(@Param("title")String title);
+
+
+    @Query("select  p from Post p where p.title = ?1")
+    Post findByPostTitle2(String title);
 
 }
